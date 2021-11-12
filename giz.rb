5f34f36f37c246d1b09f838abe65294378ced8c6
GizemDuzdasi <21060618@stu.omu.edu.tr>
2021-11-12 15:36:41 +0300

    commit

---
+++ giz.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+print 'Gizemin yaşını girirniz:'
+a = gets.chomp.to_i
+print 'Esranın yaşını giriniz:'
+b = gets.chomp.to_i
+print 'Mustafanın yaşını girin:'
+c = gets.chomp.to_i
+puts (a + b + c) / 3
